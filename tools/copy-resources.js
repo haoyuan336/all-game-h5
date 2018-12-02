b0065d508474e96cf89d21c166a3f8afc1329c8c
楚浩远 <328387458@qq.com>
2018-12-02 10:50:35 +0800

    copy资源的工具

---
+++ tools/copy-resources.js
@@ -0,0 +1,65 @@
+let path = process.argv[2];
+let fs = require('fs');
+let files = fs.readdirSync(path);
+var exec = require('child_process').exec;
+console.log('files ', files);
+
+//取出image 目录
+const getImagePath = function (ph) {
+    let fis = fs.readdirSync(path + '/' + ph);
+    for (let i = 0; i < fis.length; i++) {
+        let fn = fis[i];
+        if (fn.indexOf('image') != -1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+let pathList = [];
+
+for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+    if (file.indexOf('.') == -1 && getImagePath(file)) {
+        pathList.push(file);
+
+    }
+}
+
+function processStr(cmd, cb) {
+    console.log('cmd = ' + cmd);
+    exec(cmd, function (err, stdout, stderr) {
+        if (err) {
+            console.log('err' + err);
+        } else {
+            if (cb) {
+                cb()
+            }
+        }
+    });
+}
+const createDir = function (dir, cb) {
+    processStr('mkdir ' + dir, cb);
+}
+processStr('rm -rf ./game-path/*', ()=>{
+    createDir('./game-path/games/', ()=>{
+        for (let i = 0; i < pathList.length; i++) {
+            createDir('./game-path/games/' + pathList[i]);
+            processStr('cp -rf ' + path  + '/'+ pathList[i] + '/images/' + ' ' + './game-path/games/' + pathList[i] + '/images', () => {
+                console.log('success');
+            })
+        }
+    });
+});
+
+
+
+processStr('cp -rf dist ./game-path/', ()=>{
+
+});
+processStr('cp -f index.html ./game-path/', ()=>{
+
+});
+processStr('scp -r ./game-path/* root@chutianba.xyz:/root/webserver/public', ()=>{
+
+});
